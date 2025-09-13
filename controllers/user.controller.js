f5d248263d8d6811a529edebff51d562f8e5d8b6
Orange1008 <swetaprachi108@gmail.com>
2025-09-13 22:43:56 +0530

    refactor registerUser function to include detailed comments for user registration process

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,19 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async(req,res)=>{
-      res.status(200).json({
-        message:"first post request"
-    })
+      //get user details
+      //validation-not empty
+      //check if already exists:username or email
+      //check for images or avatar
+      //upload to cloudinary,avatar
+      //create user object-create entry in db
+      //remove password and refresh token feild from response
+      //check for user creation
+      //return res 
+      const {fullName,email,username,password}=req.body
+      console.log("email: ",email);
+      
+      
+  
 })
 export {registerUser,}
