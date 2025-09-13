7433881316598e484eeae4767f34ac7bcfcba720
Orange1008 <swetaprachi108@gmail.com>
2025-09-13 18:37:51 +0530

    setup for post request

--- src/app.js
+++ src/app.js
@@ -12,8 +12,13 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes 
+import userRouter from "../routes/user.routes.js" 
+//route use because we are importing them
+app.use("/api/v1/users",userRouter)
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 export {app}
